4a7833452b91e1e498190226525f2763694d93c1
Hasung Jun <59468036+iianjun@users.noreply.github.com>
2025-11-12 23:07:31 +0900

    remove: Profiler

--- apps/example/app/no-virtualization/page.tsx
+++ apps/example/app/no-virtualization/page.tsx
@@ -1,38 +1,16 @@
 "use client";
-import { Profiler } from "react";
 import { NO_VIRTUALIZATION_ITEMS } from "../../constants";
 export default function Page() {
   return (
     <div>
       <h1>No virtualization</h1>
-      <Profiler
-        id="NoVirtualization"
-        onRender={(
-          id,
-          phase,
-          actualDuration,
-          baseDuration,
-          startTime,
-          commitTime
-        ) => {
-          console.log({
-            id,
-            phase,
-            actualDuration,
-            baseDuration,
-            startTime,
-            commitTime,
-          });
-        }}
-      >
-        <div style={{ width: "50vw", border: "1px solid gray" }}>
-          {NO_VIRTUALIZATION_ITEMS.map((item) => (
-            <div key={item} style={{ height: 50 }}>
-              {item}
-            </div>
-          ))}
-        </div>
-      </Profiler>
+      <div style={{ width: "50vw", border: "1px solid gray" }}>
+        {NO_VIRTUALIZATION_ITEMS.map((item) => (
+          <div key={item} style={{ height: 50 }}>
+            {item}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
